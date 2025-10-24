fa2ce06cf2e3bccb396183184ffbb5ca883613dd
shunf4 <shun1048576@gmail.com>
2025-10-24 14:37:25 +0800

    SF mod: fix font of tab control

--- CPP/Windows/Control/PropertyPage.cpp
+++ CPP/Windows/Control/PropertyPage.cpp
@@ -89,13 +89,6 @@ int CALLBACK PropSheetProc(HWND hwndDlg, UINT uMsg, LPARAM lParam)
       // Use Segoe UI if available, otherwise fall back to system message font
       g_hModernFont = CreateFontIndirectW(&ncm.lfMessageFont);
       
-      // force Segoe UI specifically:
-      LOGFONTW lf = {0};
-      lf.lfHeight = -14;
-      lf.lfWeight = FW_NORMAL;
-      wcscpy_s(lf.lfFaceName, LF_FACESIZE, L"Segoe UI");
-      g_hModernFont = CreateFontIndirectW(&lf);
-      
       // Set font for tab control
       HWND hTab = GetDlgItem(hwndDlg, 12320); // IDC_TABCONTROL, Magic number for tab control
       if (hTab && g_hModernFont)
